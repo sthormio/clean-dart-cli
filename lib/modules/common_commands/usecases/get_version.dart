e52fd4599eb296e9afd2ca35e69a33ac76e34fb3
Bruno Alves <bruno.santos@ifpi.edu.br>
2020-12-08 14:37:06 -0300

    fix bug get app version

--- lib/modules/common_commands/usecases/get_version.dart
+++ lib/modules/common_commands/usecases/get_version.dart
@@ -1,15 +1,11 @@
-import 'dart:io';
-import 'package:path/path.dart' as p;
+import '../../../shared/constants.dart';
 import '../../../shared/interfaces/iget_version_cli.dart';
 import '../../../shared/utils/output_utils.dart' as output;
 
 class GetVersionCli implements IGetVersionCli {
   @override
   Future<bool> call() async {
-    var pathNomalized = p.normalize('${p.current}/pubspec.yaml');
-    var file = await File(pathNomalized);
-    var content = await file.readAsLines();
-    output.title('Clean Dart CLI at ${content[2]}');
+    output.title('Clean Dart CLI at ${packageVersion}');
     return true;
   }
 }
