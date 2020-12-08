0ab7cfe72ae8efc3d781c329ba62accb258226c8
Bruno Alves <bruno.santos@ifpi.edu.br>
2020-12-08 10:39:37 -0300

    adding example

---
+++ example/domain/usecases/register_user_usecase.dart
@@ -0,0 +1,14 @@
+abstract class IRegisterUser {
+  Future<void>call();
+}
+
+
+class RegisterUser implements IRegisterUser {
+  @override
+  Future<void> call() {
+    // TODO: implement call
+    throw UnimplementedError();
+  }
+  
+}
+  
